a309c44590329aea11a76e809c0798df362a26de
NicoleOrmerod94 <147579584+NicoleOrmerod94@users.noreply.github.com>
2023-10-25 10:42:57 -0600

    completed

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution in this file!
+// Define the 'employee' object
+let employee = {
+    // Properties of the employee object
+    name: 'Sam',
+  };
+  
+  // Define the 'updateEmployeeWithKeyAndValue' function
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    // Create a clone of the 'employee' object to avoid modifying the original
+    const updatedEmployee = { ...employee };
+    
+    // Update the clone with the new key-value pair
+    updatedEmployee[key] = value;
+    
+    // Return the updated clone
+    return updatedEmployee;
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    // Update the 'employee' object destructively with the given key and value
+    employee[key] = value;
+    return employee;
+  }
+
+  function deleteFromEmployeeByKey(employee, key) {
+    // Create a clone of the 'employee' object without the specified key
+    const updatedEmployee = { ...employee };
+    delete updatedEmployee[key];
+    return updatedEmployee;
+  }
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    // Delete the specified key from the 'employee' object destructively
+    delete employee[key];
+    return employee;
+  }
+  
